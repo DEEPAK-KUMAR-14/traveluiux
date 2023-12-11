f01d27b23636cfbd8202270ce5a662e0160241bc
DEEPAK KUMAR <malideepakkumar012@gmail.com>
2023-12-11 17:05:30 +0530

    Initial Commit

---
+++ components/Button.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+type ButtonProps = {
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
+  href?: string; // Added href prop for navigation
+};
+
+const Button = ({ type, title, icon, variant, full, href }: ButtonProps) => {
+  if (href) {
+    return (
+      <Link href={href}>
+        <a>
+          <button
+            className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+            type={type}
+          >
+            {icon && <Image src={icon} alt={title} width={24} height={24} />}
+            <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+          </button>
+        </a>
+      </Link>
+    );
+  }
+
+  // Handle the case where href is not provided
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+const nextConfig = {
+  // Other configuration options can go here
+};
+
+module.exports = nextConfig;
